b699db98edb2e2529f3aa91ba04a4e6876a3409e
Thomasnbawa <Thomas666163@163.com>
2025-04-03 12:15:07 +0800

    Update daylight.py

--- code/SeniorOS/system/daylight.py
+++ code/SeniorOS/system/daylight.py
@@ -17,9 +17,7 @@ def UITime(pages=True):
     return f'{t[3]:02}{':' if pages else ""}{t[4]:02}'
 
 #GetCharWidth = lambda s: oled.DispChar(s, 0, 0, Colormode.noshow)[0][0] + int(len(s)/2)
-def GetCharWidth(s):
-    width = oled.DispChar(s, 0, 0, Colormode.noshow)  # 先获取返回值
-    return width[0][0] + int(len(s)/2)  # 再计算宽度
+GetCharWidth = lambda s: oled.DispChar(s, 0, 0, 0)[0][0] + int(len(s)/2)
 
 AutoCenter = lambda string: 64 - GetCharWidth(string) // 2
 HomeTimeAutoCenter = AutoCenter
